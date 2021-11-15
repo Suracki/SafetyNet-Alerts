060fb540db6acebf1f824dfced126286664fb2d0
Simon Linford <simon.linford@gmail.com>
2021-11-15 16:06:03 +0100

    updated healthcheck to show details

---
+++ src/main/java/com/safetynet/alerts/presentation/actuator/HealthCheck.java
@@ -0,0 +1,30 @@
+package com.safetynet.alerts.presentation.actuator;
+
+import com.safetynet.alerts.configuration.DataConfig;
+import com.safetynet.alerts.data.io.JsonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class HealthCheck implements HealthIndicator {
+
+    @Autowired
+    JsonDAO jsonDAO;
+
+    @Autowired
+    DataConfig dataConfig;
+
+    @Override
+    public Health health() {
+        try {
+            jsonDAO.readJsonFromFile(dataConfig.getDataFile());
+        }
+        catch (Exception e) {
+            return Health.down().withDetail("Cause", "Data file" + dataConfig.getDataFile() + " is not available.").build();
+        }
+        return Health.up().build();
+    }
+}
